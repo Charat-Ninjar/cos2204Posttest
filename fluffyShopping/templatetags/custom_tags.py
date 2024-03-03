17d97406d0ce55127c4ecad3e916b2a63f6ee9e5
Charat-Ninjar <charatvc@gmail.com>
2024-03-03 15:09:22 +0700

    add data product mockup

--- fluffyShopping/templatetags/custom_tags.py
+++ fluffyShopping/templatetags/custom_tags.py
@@ -8,8 +8,8 @@ def productCard(context):
     # Implement your logic here to fetch product data and render the HTML for the card
     # For demonstration purposes, let's assume you have a list of products
     products = [
-        {'name': 'Product 1', 'description': 'Description of Product 1', 'price': 10},
-        {'name': 'Product 2', 'description': 'Description of Product 2', 'price': 20},
-        {'name': 'Product 3', 'description': 'Description of Product 3', 'price': 30},
+        {'name': 'Product 1', 'description': 'Description of Product 1', 'price': 10,'image_url': 'https://www.mariskavos.nl/wp-content/uploads/2023/06/free-alpaca-crochet-pattern-2.webp'},
+        {'name': 'Product 2', 'description': 'Description of Product 2', 'price': 20, 'image_url': 'https://www.mariskavos.nl/wp-content/uploads/2023/06/free-alpaca-crochet-pattern-2.webp'},
+        {'name': 'Product 3', 'description': 'Description of Product 3', 'price': 30, 'image_url': 'https://www.mariskavos.nl/wp-content/uploads/2023/06/free-alpaca-crochet-pattern-2.webp'},
     ]
     return render_to_string('product_card_template.html', {'card_collection': products})
